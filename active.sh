ada6c40b91cdd4b55e7e9c4e88483f0464911026
Anjas amar pradana <anjastech01@gmail.com>
2024-03-06 03:32:59 +0700

    Update active.sh

--- active.sh
+++ active.sh
@@ -17,9 +17,9 @@ rm -rf /usr/local/directadmin/conf/license.key
 wget -O /usr/local/directadmin/conf/license.key 'http://license.vsicloud.com/getLic.php'
 chmod 600 /usr/local/directadmin/conf/license.key
 chown diradmin:diradmin /usr/local/directadmin/conf/license.key
-ifconfig eth0:100 176.99.3.34 netmask 255.0.0.0 up
+ifconfig eth0:100 103.187.146.38 netmask 255.0.0.0 up
 echo 'DEVICE=eth0:100' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
-echo 'IPADDR=176.99.3.34' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
+echo 'IPADDR=103.187.146.38' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
 echo 'NETMASK=255.0.0.0' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
 service network restart
 /usr/bin/perl -pi -e 's/^ethernet_dev=.*/ethernet_dev=eth0:100/' /usr/local/directadmin/conf/directadmin.conf
